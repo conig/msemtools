54a4a7666ba2943474a5296c274f6e4e6c3a9d12
JConigrave <james.conigrave@gmail.com>
2018-12-22 15:31:19 +1100

    added moderation function

--- R/basicfunctions.R
+++ R/basicfunctions.R
@@ -7,36 +7,41 @@
 #' @export extractData
 
 extractData = function(model){
+
+  safe_add = function(x) ifelse(length(x) >0, x,NA)
+
   summary = summary(model)
   coef = summary$coefficients %>% data.frame %>% rownames_to_column
   names(coef) = c("row","est","stde","lbound","ubound","z","p")
-  names = c("name","k","n","estimate","SE","lbound","ubound","t2","t2p","I2","t2_3","t2_3p",
-            "I2_3","Q","Q_p","slope","slope_lbound","slope_ubound","R2_2","R2_3","Mx_status")
+  names = c("model.name","k","n","estimate","SE","lbound","ubound","t2","t2p","I2","t2_3","t2_3p",
+            "I2_3","Q","Q_p","slope","slope_lbound","slope_ubound","slope_p","R2_2","R2_3","Mx_status")
   result = data.frame(matrix(ncol = length(names),nrow = 1))
   names(result) = names
   result$k = summary$no.studies
   result$n = summary$obsStat
-  result$estimate = coef[coef$row == "Intercept","est"]
-  result$SE = coef[coef$row == "Intercept","stde"]
-  result$lbound = coef[coef$row == "Intercept","lbound"]
-  result$ubound = coef[coef$row == "Intercept","ubound"]
-  result$t2 = coef[coef$row == "Tau2_2","est"]
-  result$t2p = coef[coef$row == "Tau2_2","p"]
-  result$t2_3 = coef[coef$row == "Tau2_3","est"]
-  result$t2_3p = coef[coef$row == "Tau2_3","p"]
-  result$Q = summary$Q.stat$Q
-  result$Q_p = summary$Q.stat$pval
+
+  result$estimate = safe_add(coef[coef$row == "Intercept","est"])
+  result$SE = coef[coef$row == "Intercept","stde"] %>%
+    safe_add
+  result$lbound = coef[coef$row == "Intercept","lbound"] %>% safe_add
+  result$ubound = coef[coef$row == "Intercept","ubound"] %>% safe_add
+  result$t2 = coef[coef$row == "Tau2_2","est"] %>% safe_add
+  result$t2p = coef[coef$row == "Tau2_2","p"] %>% safe_add
+  result$t2_3 = coef[coef$row == "Tau2_3","est"]%>% safe_add
+  result$t2_3p = coef[coef$row == "Tau2_3","p"]%>% safe_add
+  result$Q = summary$Q.stat$Q%>% safe_add
+  result$Q_p = summary$Q.stat$pval %>% safe_add
   if(!all(is.na(summary$I2.values))){
-  result$I2 = summary$I2.values[1,2]
-  result$I2_3 = summary$I2.values[2,2]
+  result$I2 = summary$I2.values[1,2] %>% safe_add
+  result$I2_3 = summary$I2.values[2,2] %>% safe_add
   }else{
     result$I2 = NA
     result$I2_3 = NA
   }
 
   if(!all(is.na(summary$R2.values))){
-    result$R2_2 = summary$R2.values[3,1]
-    result$R2_3 = summary$R2.values[3,2]
+    result$R2_2 = summary$R2.values[3,1] %>% safe_add
+    result$R2_3 = summary$R2.values[3,2] %>% safe_add
   }
   if(!is.na(coef[coef$row == "Tau2_2","p"])){
     if(coef[coef$row == "Tau2_2","p"] < 0.05){
@@ -49,24 +54,40 @@ extractData = function(model){
   slopes = sum(grepl("Slope",coef$row))
 
   if(slopes > 0 & slopes < 2){
-    result$slope = coef[coef$row == "Slope_1","est"]
-    result$slope_lbound = coef[coef$row == "Slope_1","lbound"]
-    result$slope_ubound = coef[coef$row == "Slope_1","ubound"]
+    result$slope = coef[coef$row == "Slope_1","est"] %>% safe_add
+    result$slope_lbound = coef[coef$row == "Slope_1","lbound"] %>% safe_add
+    result$slope_ubound = coef[coef$row == "Slope_1","ubound"] %>% safe_add
+    result$slope_p = coef[coef$row == "Slope_1","p"] %>% safe_add
 
   }else{
     result$slope = NA
     result$slope_lbound = NA
     result$slope_ubound = NA
+    result$slope_p = NA
   }
 
  if(is.null(summary$call$model.name)){
    result$name = NA
  }else{
-   result$name = as.character(summary$call$model.name)
+   result$model.name = as.character(summary$call$model.name)
  }
 result$Mx_stats = summary$Mx.status1
 
 return(as_tibble(result))
+}
+
+#' extractSlopes
+#'
+#' Grabs slope coefficient data
+#'
+#' @param model a model.
+#' @export extractSlopes
+
+extractSlopes = function(model){
+  summary = summary(model)
+  coef = summary$coefficients %>% data.frame %>% rownames_to_column
+  names(coef) = c("row","est","stde","lbound","ubound","z","p")
+  coef[grepl("Slope",coef$row),]
 }
 
 #' string_meta3
@@ -78,20 +99,21 @@ return(as_tibble(result))
 #' @param x a string.
 #' @param data an object. data object
 #' @param model.name a string. The name of the model
+#' @param intercept.constraints a numeric. Set the value of the intercept
 #' @importFrom metaSEM meta3 rerun
 #'
 #' @export string_meta3
 
-#data = f ; y = "drink_yi"; v = "drink_vi";factor = "gender_cat"; names = NULL; cluster = "study_id"; x = "females_p"; model.name = "test"
 
-string_meta3 = function(y, v, cluster, x = NULL, data, model.name = NA) {
+string_meta3 = function(y, v, cluster, x = NULL, data, model.name = NA,intercept.constraints = NULL) {
 
   if (is.null(x)) {
     model = metaSEM::meta3(
       y = eval(parse(text = y)),
       v = eval(parse(text = v)),
       cluster = eval(parse(text = cluster)),
-      data = data
+      data = data,
+      intercept.constraints = intercept.constraints
     )
   } else{
     model = metaSEM::meta3(
@@ -99,12 +121,13 @@ string_meta3 = function(y, v, cluster, x = NULL, data, model.name = NA) {
       v = eval(parse(text = v)),
       x = eval(parse(text = x)),
       cluster = eval(parse(text = cluster)),
-      data = data
+      data = data,
+      intercept.constraints = intercept.constraints
     )
   }
 
   if(!summary(model)$Mx.status %in% c(0,1)){
-    model = rerun(model)
+    model = metaSEM::rerun(model)
   }
 
   model$call$model.name = model.name
@@ -121,26 +144,30 @@ string_meta3 = function(y, v, cluster, x = NULL, data, model.name = NA) {
 #' @param factor a string.
 #' @param data an object The name of the data object
 #' @param names character vector. If given, default names will be replaced.
+#' @param output.models a bool. If true, models are provided in a list.
 #' @importFrom metaSEM meta3
-#' @importFrom dplyr filter
+#' @importFrom dplyr filter %>%
 #' @importFrom tibble as_tibble
 #'
 #' @export meta3_by_factor
 
 
-meta3_by_factor = function(y, v, cluster, factor, data, names = NULL) {
+meta3_by_factor = function(y, v, cluster, factor, data, names = NULL, output.models = F) {
   #prepare factors
   . = NULL
   factor_levels = levels(data[, factor])
 
-  model_list = lapply(factor_levels, function(x) {
-    temp_data = data %>% filter(data[, factor] == x)
+  model_list = lapply(factor_levels, function(fc) {
+    #message(fc)
+    temp_data = data %>% filter(data[, factor] == fc)
     output = string_meta3(
       data = temp_data,
       y = y,
       v = v,
+      x = NULL,
       cluster = cluster,
-      model.name = x
+      model.name = fc,
+      intercept.constraints = NULL
     )
     output
   })
@@ -162,9 +189,183 @@ meta3_by_factor = function(y, v, cluster, factor, data, names = NULL) {
         )
       )
     } else{
-      result$model = names
+      results$model.name = names
     }
   }
-  results %>% as_tibble
+
+  if(output.models){
+  return(list(table = as_tibble(results), models = model_list))
+  } else {
+    return(as_tibble(results))
+  }
+
 }
 
+
+#' meta3_moderation
+#'
+#' This function streamlines the moderation process. Moderation strategy varies based on whether a numeric, or factoral variable is supplied.
+#' @param y a string. The name of yi
+#' @param v a string. The name of vi
+#' @param cluster a string. The name of the clustering variable e.g. 'study id'.
+#' @param moderators a charcater vector. The names of the variable used for moderation
+#' @param data an object.
+#' @param base a meta3 model. Defaults to NULL. The baseline model is automatically calculated, but can also be supplied with this argument.
+#' @param names a vector of strings. If supplied, the models can be renamed.
+#' @importFrom metaSEM meta3
+#' @importFrom dplyr filter %>% add_row
+#' @importFrom tibble as_tibble
+#' @importFrom stats anova
+#' @export meta3_moderation
+
+#y = "drink_yi"; v = "drink_vi"; cluster = "study_id"; moderators = c("gender_cat","females_p"); data = f; base = NULL; names = NULL
+# msemtools::meta3_moderation("drink_yi","drink_vi","study_id","gender_cat",data = f)
+#x = msemtools::meta3_moderation("drink_yi","drink_vi","study_id",c("gender_cat"),data = f)
+meta3_moderation = function(y,
+                            v,
+                            cluster,
+                            moderators,
+                            data,
+                            base = NULL,
+                            names = NULL) {
+
+  if (is.null(base)) {
+
+    base = string_meta3(
+      data = data,
+      y = y,
+      v = v,
+      x = NULL,
+      cluster = cluster,
+      model.name = "baseline",
+      intercept.constraints = NULL
+    )
+
+  }
+
+  get_vars = function(x){
+    x %>% dplyr::select(
+      moderation,
+      model.name,
+      k,
+      n,
+      estimate,
+      lbound,
+      ubound,
+      I2,
+      I2_3,
+      slope,
+      slope_lbound,
+      slope_ubound,
+      R2_2,
+      R2_3
+    )
+  }
+
+  base_table = base %>% extractData %>%
+    dplyr::mutate(moderation = "baseline") %>%
+    get_vars %>%
+    dplyr::mutate("anova p-value" = NA) %>%
+    list
+
+  amazing_result = lapply(moderators, function(x) {
+    mod = data[, x]
+    if (!(is.numeric(mod) | is.factor(mod))) {
+      stop(
+        paste0(
+          "'",
+          x,
+          "' is a ",
+          class(mod),
+          " variable. Moderators must be numeric or factor variables."
+        )
+      ,call.=F)
+    }
+
+    if (is.numeric(mod)) {
+      message("numeric")
+      temp_model = string_meta3(
+        y = y,
+        v = v,
+        cluster = cluster,
+        x = x,
+        data = data,
+        model.name = x
+      )
+      temp_anova = anova(temp_model, base)
+      temp_table = extractData(temp_model) %>%
+        dplyr::mutate(moderation = x) %>%
+        get_vars %>%
+        dplyr::mutate("anova p-value" = temp_anova$p[2])
+      return(temp_table)
+    }
+
+
+    if (is.factor(mod)) {
+      message("Factor")
+      levels = levels(mod) #we remove the first level as it becomes the reference group
+      cat_x = lapply(levels, function(y) {
+        ifelse(mod == y, 1, 0)
+      }) %>%
+        do.call("cbind", .)
+      colnames(cat_x) = levels
+
+      ##sting meta won't take cat_x due to its environment.
+      #so we will mix up this approach.
+      data$y_internal = data[,y]
+      data$v_internal = data[,v]
+      data$cluster_internal = data[,cluster]
+
+      temp_model = metaSEM::meta3(
+        y = y_internal,
+        v = v_internal,
+        cluster = cluster_internal,
+        x = cat_x,
+        data = data,
+        model.name = "temp_name",
+        intercept.constraints = 0
+      )
+      temp_model$call$model.name = x
+      temp_anova = anova(temp_model, base)
+      temp_slopes = temp_model %>% extractSlopes
+      temp_slopes$row = colnames(cat_x)
+      temp_by_cat = meta3_by_factor(
+        y = y,
+        v = v,
+        cluster = cluster,
+        factor = x,
+        data = data
+      )
+      temp_table = extractData(temp_model) %>%
+        dplyr::mutate(moderation = x) %>%
+        get_vars %>%
+        dplyr::mutate("anova p-value" = temp_anova$p[2])
+      if (nrow(temp_by_cat) != nrow(temp_slopes))
+        warning(paste0("rows, "))
+      for (i in seq_len(nrow(temp_slopes))) {
+        temp_table = temp_table %>%
+          add_row(
+            moderation = x,
+            model.name = temp_slopes$row[i],
+            #add a space to indent line.
+            k = temp_by_cat$k[i],
+            n = temp_by_cat$n[i],
+            estimate = temp_slopes$est[i],
+            lbound = temp_slopes$lbound[i],
+            ubound = temp_slopes$ubound[i],
+          )
+      }
+      return(temp_table)
+    }
+
+
+  })
+
+  amazing_result = append(base_table, amazing_result) %>%
+    do.call("rbind", .)
+
+  return(amazing_result)
+}
+
+utils::globalVariables(c(".","I2","I2_3","R2_2","R2_3","anova","estimate",
+                         "k","n" ,"lbound","ubound","slope","slope_ubound","result","model.name"))
---
+++ R/plotting-functions.R
@@ -0,0 +1,59 @@
+#' extractSlopes
+#'
+#' Grabs slope coefficient data
+#'
+#' @param model a model.
+#' @export extractSlopes
+
+
+ninjaForest = function(model,summary = NULL,save = F,height = 16, width = 16,f_name = test.png, title = ""){
+  #this chunk sets up summary object
+  summary = extractData(model) %>%
+    select(p,lbound,ubound)
+  names(summary) = c("p","lower","upper")
+  summary$type = "summary"
+  summary$setting = "pooled"
+  summary$year = 1
+  summary$cluster = "pooled"
+
+  dat = model$data %>%
+    mutate(se = sqrt(v),
+           lower = logit2prob(y - 1.96*se),
+           upper = logit2prob(y + 1.96*se)) %>%
+    mutate(y = logit2prob(y), type = "data", setting = "effects") %>%
+    dplyr::select(p = y, lower,upper,type,setting, cluster)
+
+  dat$author = lapply(dat$cluster, function(x){
+    x = as.numeric(as.character(x))
+    f[f$study_id == x, "author"] %>% unlist() %>% .[1]
+  }) %>% unlist
+
+  dat$year = lapply(dat$cluster, function(x){
+    x = as.numeric(as.character(x))
+    f[f$study_id == x, "year"] %>% unlist() %>% .[1]
+  }) %>% unlist
+  dat = dat[order(dat$year),]
+  dat$cluster = paste(dat$author,dat$year)
+  #create empty columns in summary for author and year
+  fun = dat[,c("cluster","p","lower","upper","type","setting","year")]
+  dat = rbind(fun,
+              summary)
+  dat=dat[order(dat$year),]
+
+  test = ggplot(dat, aes(y = reorder(cluster,year), x = p, xmin = lower, xmax = upper)) + geom_point(color = "black") +
+    geom_point(data = dat[dat$type == "summary",],color = 'black', shape = 18, size = 4.5)+
+    #add the CI error bars
+    geom_errorbarh(height=.1)+
+    #Specify the limits of the x-axis and relabel it to something more meaningful
+    scale_x_continuous(name='proportion')+
+    ylab(NULL)+
+    ggtitle(title)+
+    geom_vline(xintercept=.5, color='black', linetype='dashed') +
+    facet_grid(setting~., scales= 'free', space='free')+
+    theme_classic()
+  if(save == T){
+    ggsave(f_name, path = getwd(), height = height, width = height, units = "cm", dpi = 600)
+
+  }
+  test
+}
