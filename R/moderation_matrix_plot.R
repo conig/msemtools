f01d5dc9fcee8983b6a51b7e90422ad547862d15
conig <james.conigrave@gmail.com>
2020-10-13 19:50:14 +1100

    darken non-sig

--- R/moderation_matrix_plot.R
+++ R/moderation_matrix_plot.R
@@ -68,7 +68,7 @@ moderation_matrix <- function(..., effect_size = "Effect size", moderators = NUL
               fill = "black",
               xmin = -Inf, xmax = Inf,
               ymin = -Inf, ymax = Inf,
-              alpha = 0.05, inherit.aes = F) + # inherit.aes caused factors to lose order.
+              alpha = 0.15, inherit.aes = F) + # inherit.aes caused factors to lose order.
     ggplot2::theme_bw() + geom_vline(xintercept = null_value, linetype = 2) + # add in vertical line at 0
     ggplot2::geom_point() + geom_errorbarh(height = .1)+
     ggplot2::facet_grid( # grid by moderation and outcome
@@ -91,6 +91,6 @@ moderation_matrix <- function(..., effect_size = "Effect size", moderators = NUL
 # mod2 = meta3(risk_short_yi, risk_short_vi, study_id, data = msemtools::conigrave20) %>%
 #   moderate(Gender, Age, Cohort)
 # #
-# msemtools:::moderation_matrix("Current drinker" =  mod1,
+# moderation_matrix("Current drinker" =  mod1,
 #                   "Short term risk" =  mod2)
 # summary(mod1)
